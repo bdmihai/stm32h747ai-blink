a422c3345c8e4410044104db5345dec83139f998
Mihai <bdmihai@gmail.com>
2024-02-09 20:01:55 +0100

    initial revision

---
+++ source/app/gpio.cpp
@@ -0,0 +1,61 @@
+/*_____________________________________________________________________________
+ │                                                                            |
+ │ COPYRIGHT (C) 2023 Mihai Baneu                                             |
+ │                                                                            |
+ | Permission is hereby  granted,  free of charge,  to any person obtaining a |
+ | copy of this software and associated documentation files (the "Software"), |
+ | to deal in the Software without restriction,  including without limitation |
+ | the rights to  use, copy, modify, merge, publish, distribute,  sublicense, |
+ | and/or sell copies  of  the Software, and to permit  persons to  whom  the |
+ | Software is furnished to do so, subject to the following conditions:       |
+ |                                                                            |
+ | The above  copyright notice  and this permission notice  shall be included |
+ | in all copies or substantial portions of the Software.                     |
+ |                                                                            |
+ | THE SOFTWARE IS PROVIDED  "AS IS",  WITHOUT WARRANTY OF ANY KIND,  EXPRESS |
+ | OR   IMPLIED,   INCLUDING   BUT   NOT   LIMITED   TO   THE  WARRANTIES  OF |
+ | MERCHANTABILITY,  FITNESS FOR  A  PARTICULAR  PURPOSE AND NONINFRINGEMENT. |
+ | IN NO  EVENT SHALL  THE AUTHORS  OR  COPYRIGHT  HOLDERS  BE LIABLE FOR ANY |
+ | CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER IN AN ACTION OF CONTRACT, TORT |
+ | OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  |
+ | THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                 |
+ |____________________________________________________________________________|
+ |                                                                            |
+ |  Author: Mihai Baneu                           Last modified: 29.Dec.2023  |
+ |                                                                            |
+ |___________________________________________________________________________*/
+ 
+#include "stm32h7xx.h"
+#include "stm32h7xx_hal.h"
+#include "gpio.h"
+
+void gpio_init()
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : PE3 */
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+}
+
+void gpio_set_led()
+{
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
+}
+
+void gpio_reset_led()
+{
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
+}
